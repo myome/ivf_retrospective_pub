7a73fc786ab70eddff90fc4acfcd56d88420d433
Kate Im <kim@myome.com>
2021-09-21 21:39:39 +0000

    Figure updates

--- figures/Fig1C.R
+++ figures/Fig1C.R
@@ -14,6 +14,6 @@ prscovbygt<-ggplot(prstats, aes(fill=gt, y=cov*100, x=index)) + geom_bar(positio
 prserrorbygt<-ggplot(prstats, aes(fill=gt, y=(1-error)*100, x=index)) + geom_bar(position="dodge", stat="identity") + ylab("Accuracy (%)") + xlab("Case ID")+scale_fill_manual(values = c("#999999","#666666","black")) + theme(legend.key.size = unit(0.25, 'cm'),legend.text=element_text(size=6), axis.text=element_text(size=6),axis.title=element_text(size=7),panel.grid.major.y = element_line(size=0.25, color="gray"), panel.grid.major.x = element_blank(),panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.title=element_blank(),axis.text.x = element_text(angle = 45)) + coord_cartesian(ylim = c(90, 100))
 
 #Put together in grid
-combinedplot<-ggarrange(cov, prscovbygt,error,prserrorbygt,ncol=2,nrow=2,  common.legend=TRUE, labels=c("Genome-wide", "Polygenic model sites"), font.label=list(color="black", size=6),hjust=-0.65)
-ggsave("./figures/coverage_error_bygt.png", plot=combinedplot, dpi=300, width=3, height=3, units=c("in"))
+combinedplot<-ggarrange(cov, prscovbygt,error,prserrorbygt,ncol=2,nrow=2,  common.legend=TRUE, legend=c("bottom"), labels=c("Genome-wide", "Polygenic model sites"), font.label=list(color="black", size=6),hjust=-0.65)
+ggsave("./figures/coverage_error_bygt.png", plot=combinedplot, dpi=300, width=4.4, height=4.8, units=c("in"))
 dev.off()
--- figures/SuppFigure4.R
+++ figures/SuppFigure4.R
@@ -6,7 +6,7 @@ oddsratios<-read.table("./data/retro.or.lifetimerisk.txt", header=T)
 png("./figures/suppfig4a.png", width=1000, height=500)
 
 #Generate plots for each phenotype with data for each family
-ggplot(oddsratios,aes(x=as.factor(caseid),y=oddsratio,fill=phenotype),show.legend=FALSE) + geom_point(data=oddsratios,aes(x=as.factor(caseid),y=as.numeric(oddsratio)),shape = 21,size=2, color="black",alpha=.31,show.legend=FALSE) + facet_wrap(~ phenotype, ncol=4, scales = "free_y")+theme(panel.border = element_rect(colour = "black", fill=NA, size=0.1),strip.background = element_blank()) + theme_bw()+ylab("Odds ratio") + xlab("Case ID")+geom_blank(data=oddsratios, aes(x=0, y=6))+theme(text = element_text(size=15))
+ggplot(oddsratios,aes(x=as.factor(caseid),y=oddsratio,fill=phenotype),show.legend=FALSE) + geom_point(data=oddsratios,aes(x=as.factor(caseid),y=as.numeric(oddsratio)),shape = 21,size=2, color="black",alpha=.31,show.legend=FALSE) + facet_wrap(~ phenotype, ncol=4, scales = "free_y")+theme(panel.border = element_rect(colour = "black", fill=NA, size=0.1),strip.background = element_blank()) + theme_bw()+ylab("Odds ratio") + xlab("Case ID")+geom_blank(data=oddsratios, aes(x=0, y=8))+theme(text = element_text(size=15))
 dev.off()
 
 
