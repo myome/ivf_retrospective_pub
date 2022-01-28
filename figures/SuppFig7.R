69401267bd67e9e1da866a62c6ac20ffa7d7bcd0
Kate Im <kim@myome.com>
2022-01-28 14:37:43 +0000

    Add supp figure 7 script, data and pdf

---
+++ figures/SuppFig7.R
@@ -0,0 +1,8 @@
+library(ggpubr)
+library(ggplot2)
+
+simdat<-read.table("./data/embryo_sims_or.txt", header=T)
+
+histplot<-ggplot(simdat, aes(odds_ratio))+geom_histogram(bins=20, fill="blue")+facet_wrap(phenotype ~ type, ncol=4, scales="free")+ xlab("Odds ratio") + xlab("Count") + theme(panel.background = element_blank(),axis.line = element_line(colour = "black"))
+
+ggsave("./figures/SuppFig7.pdf", plot=histplot, dpi=300, width=180, height=280, units=c("mm"), device="pdf")
